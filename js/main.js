6b32ea50cd4b5aa053cdce739b33e1f66dccb017
mhmdkhaz <mhmdkh0957@gmail.com>
2022-07-20 12:03:46 +0300

    test swiper

--- js/main.js
+++ js/main.js
@@ -118,3 +118,60 @@ let swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
 });
+
+if ($(".swiper-container").length) {
+  let mySwiper = new Swiper(".swiper-container", {
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      300: {
+        mousewheel: true,
+        keyboard: true,
+        slidesPerView: 1,
+        spaceBetween: 24,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+      },
+      400: {
+        slidesPerView: 1,
+        spaceBetween: 24,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 56,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+      },
+      960: {
+        slidesPerView: 2,
+        spaceBetween: 56,
+        allowSlidePrev: true,
+        allowSlideNext: true,
+      },
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 28,
+        allowSlidePrev: false,
+        allowSlideNext: false,
+      },
+    },
+  });
+  //Reload swiper
+  $(window).resize(function () {
+    mySwiper.update();
+  });
+  $(window).on("load", function () {
+    mySwiper.update();
+  });
+}
